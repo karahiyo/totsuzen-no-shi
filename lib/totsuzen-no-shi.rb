7c28944cd36b40cab7aaf48c85b7811ecdc83252
karahiyo <a.ryuklnm@gmail.com>
2013-10-17 15:59:28 +0900

    fix spec path.

--- lib/totsuzen-no-shi.rb
+++ lib/totsuzen-no-shi.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-require './totsuzen_no_shi/generate'
+require 'totsuzen_no_shi/generate'
 
 class String
   def generate
--- spec/totsuzen-no-shi_spec.rb
+++ spec/totsuzen-no-shi_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-require 'totsuzen_no_shi'
+require 'totsuzen-no-shi'
 
 describe "TotsuzenNoShi" do
   it "あれが生成されるか_一行" do
